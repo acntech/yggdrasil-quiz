7cd3dbe1e13754b238d26d44ecdf7bf6b008e099
Lars-Olof Moilanen <lars.olof.moilanen@gmail.com>
2014-03-30 13:36:05 +0200

    Rough draft of questionnaire

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,9 +12,15 @@ angular.module('yggdrasilQuizApp', [
         templateUrl: 'partials/main',
         controller: 'MainCtrl'
       })
+      .when('/success', {
+        templateUrl: 'partials/success'
+      })
+      .when('/sorry', {
+        templateUrl: 'partials/sorry'
+      })
       .otherwise({
         redirectTo: '/'
       });
-      
+
     $locationProvider.html5Mode(true);
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,35 @@
 'use strict';
 
 angular.module('yggdrasilQuizApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+  .controller('MainCtrl', function ($scope, $http, $location) {
+    $http.get('/api/questions').success(function (questions) {
+      $scope.questions = questions;
     });
+
+    $scope.formCompleted = function () {
+      var completed = true;
+      angular.forEach($scope.questions, function (question) {
+        if (!question.alternative) {
+          completed = false;
+        }
+      });
+      return completed;
+    };
+
+    $scope.submit = function () {
+      var allCorrect = true;
+      angular.forEach($scope.questions, function (question) {
+        var alternative = JSON.parse(question.alternative);
+        if (!alternative.correct) {
+          console.log('Incorrect response: ' + JSON.stringify(alternative));
+          allCorrect = false;
+        }
+      });
+
+      if (allCorrect) {
+        $location.path('/success');
+      } else {
+        $location.path('/sorry');
+      }
+    };
   });
